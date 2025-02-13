61c0de7f2356034486496c191caf778b3df00570
Dredgegdix <113450167+Dredgegdix@users.noreply.github.com>
2025-02-13 23:31:31 +0400

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
 const title = document.querySelector('.title')
-const text = 'Надо бы поздравить детский сад с праздником🎉'.split('')
+const text = 'Время поздравлять дитя'.split('')
 for (let index = 0; index < text.length; index++) {
   if (text[index] !== ' ') {
     title.innerHTML += `<span>${text[index]}<span/>`
